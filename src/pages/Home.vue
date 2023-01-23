baea0b4d584cb5ab4f9c38fe795dfbccd5186241
Silvestre Romeo <silvestreromeo@silvestres-mbp.mynet>
2023-01-23 09:04:20 +0000

    to do app developed.

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { io } from "socket.io-client";
+
+const socket = io("127.0.0.1:3000");
+socket.on("connect", () => {
+  console.log(`Connection established with id: ${socket.id}`);
+});
+
+
+</script>
+
+<template>
+  <div class="menu-container">
+    <h1>Join the board!</h1>
+  <p>Username: {{ username }}</p>
+	<input class="username-input" v-model="username" placeholder="" />
+  <br>
+  <button class="join-button" @click="$router.push('Board')">Join</button>
+</div>
+</template>
